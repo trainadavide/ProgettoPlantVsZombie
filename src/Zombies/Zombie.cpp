54933f57bf62ae25ec66c41e1dcf4113f6e9710b
Nayla.k <nayla.kouider@stud.unifi.it>
2022-07-26 18:24:16 +0200

    added zombie class

---
+++ src/Zombies/Zombie.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 0260863 on 26/07/2022.
+//
+
+#include "Zombie.h"
---
+++ src/Zombies/Zombie.h
@@ -0,0 +1,14 @@
+//
+// Created by 0260863 on 26/07/2022.
+//
+
+#ifndef PROJECTPVSZ_ZOMBIE_H
+#define PROJECTPVSZ_ZOMBIE_H
+
+
+class Zombie {
+
+};
+
+
+#endif //PROJECTPVSZ_ZOMBIE_H
