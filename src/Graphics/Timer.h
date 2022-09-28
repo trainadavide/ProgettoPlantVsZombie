92e2b8f22450c6c52d399d46cc11284eb3643a54
trainadavide <davide.traina@stud.unifi.it>
2022-09-28 09:56:06 +0200

    Timer fixed

---
+++ src/Graphics/Timer.h
@@ -0,0 +1,24 @@
+//
+// Created by david on 28/09/2022.
+//
+
+#ifndef PLANTVSZOMBIE_TIMER_H
+#define PLANTVSZOMBIE_TIMER_H
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace sf;
+
+class Timer {
+public:
+    Timer();
+    ~Timer();
+    int getTime();
+
+private:
+    Clock* timer;
+
+};
+
+
+#endif //PLANTVSZOMBIE_TIMER_H
